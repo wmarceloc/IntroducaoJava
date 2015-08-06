c6b36a4ef657db651200dc5a3e2d31fcff51a230
sabado <sabado@I5-2013-03.aluno.local>
2015-08-06 20:07:57 -0300

    Versão 1.0 Inicializando o GitHub - Projeto
    Versão 1.1
    
    Trabalhando com classes em Java
    
    Versão 1.2
    
    Trabalhando ainda
    
    Versão 1.3

--- src/introducaojava/IntroducaoJava.java
+++ src/introducaojava/IntroducaoJava.java
@@ -4,19 +4,64 @@
  * and open the template in the editor.
  */
 package introducaojava;
-import java.util.Scanner;
+
+import java.io.IOException;
+//import java.util.Scanner;
 /**
  *
  * @author sabado
  */
 public class IntroducaoJava {
 
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws IOException{
         
-        Aves objetoPassaro = new Aves();
-        String resultado = objetoPassaro.Comer("mais");
-        if (resultado == "mais"){
-            System.out.println("Vou enviar mais comida");
+     
+        char choice;
+        
+        System.out.println("Help on");
+        System.out.println(" 1.if");
+        System.out.println(" 2.switch");
+        System.out.println("Choose one:");
+        choice = (char)System.in.read();
+        System.out.println("\n");
+        
+        switch (choice){
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition)statement");
+                System.out.println("else statement");
+                break;
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch (expression){");
+                System.out.println(" case constant:");
+                System.out.println(" statement sequence");
+                System.out.println(" break;");
+                System.out.println(" //...");
+                System.out.println("}");
+                break;
+            case '3':
+                double earthweight;
+                double moonweight;
+                
+                earthweight = 165;
+                moonweight = earthweight*0.17;
+                
+                System.out.println(earthweight + " earth-pounds in equivalent to " + moonweight + " moon-pounds");
+                
+            default:
+                System.out.println("Selection not found");
+                     
+        }
+        
+    }
+
+        //AULA DE QUARTA-FEIRA
+//        Aves objetoPassaro = new Aves();
+//        String resultado = objetoPassaro.Comer("mais");
+//        if (resultado == "mais"){
+//            System.out.println("Vou enviar mais comida");
         }
         
 //    Scanner teste = new Scanner (System.in);
@@ -33,4 +78,3 @@ public class IntroducaoJava {
         
         
     }
-}
