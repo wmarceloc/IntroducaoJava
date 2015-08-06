b76e6bc37d7482146fe91e86fb4fb174dc97a43a
sabado <sabado@I5-2013-03.aluno.local>
2015-08-05 21:02:47 -0300

    Versão 1.0 Inicializando o GitHub - Projeto
    Versão 1.1
    
    Trabalhando com classes em Java
    
    Versão 1.2

---
+++ src/introducaojava/Animal.java
@@ -0,0 +1,18 @@
+
+package introducaojava;
+
+public class Animal {
+String nome;
+int idade;
+double peso;
+String CorDosOlhos;
+
+public int Comer ( ){
+ return 1;
+}       
+
+public int Dormir (){
+    return 1;
+}
+
+}
---
+++ src/introducaojava/Aves.java
@@ -0,0 +1,11 @@
+
+package introducaojava;
+
+public class Aves {
+    String CorDasPenas = null;
+    int TamanhoDasPernas;
+    
+    public int BotarOvo (){
+        return 1;
+    }
+}
