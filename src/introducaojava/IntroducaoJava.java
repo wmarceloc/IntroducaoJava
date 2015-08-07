bc9ab1c179feeb88d12ed7b9b6d6fa4dba40850e
sabado <sabado@I5-2013-03.aluno.local>
2015-08-06 21:05:24 -0300

    Versão 1.0 Inicializando o GitHub - Projeto
    Versão 1.1
    
    Trabalhando com classes em Java
    
    Versão 1.2
    
    Trabalhando ainda
    
    Versão 1.3

--- src/introducaojava/IntroducaoJava.java
+++ src/introducaojava/IntroducaoJava.java
@@ -102,7 +102,42 @@ public class IntroducaoJava {
              
             default:
                 System.out.println("Selection not found");
-                     
+             
+            case '7':
+                double gallons;
+                double liters;
+                
+                gallons = 10; //start with 10 galons
+                liters = gallons * 3.7854; // convert to liters
+                
+                System.out.println(gallons +" gallons is " + liters + " liters");
+                
+            case '8':
+                //listing 12 demonstrate the relational and logical operator
+                
+                int j;
+                boolean b1, b2;
+                
+                i = 10;
+                j = 11;
+                if(i<j)System.out.println("i<j");
+                if(i<=j)System.out.println("i<=j");
+                if(i!=j)System.out.println("i!=j");
+                if(i==j)System.out.println("This won't execute");
+                if(i>=j)System.out.println("This won't execute");
+                if(i>j)Syste.out.println("This won't execute");
+                
+                b1=true;
+                b2=false;
+                
+                if(b1 & b2) System.out.println("This won't execute");
+                if(!(b1 & b2))System.out.println("!(b1 & b2) is true");
+                if(b1 | b2) System.out.println("b1 | b2 is true");
+                if(b1 ^ b2)System.out.println("b1 ^ b2 is true");
+                       
+                
+                
+                   
         }
         
     }
